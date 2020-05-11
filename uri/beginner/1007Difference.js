276047f1f271ad5e00012cab13d6c5811c2f3833
saroarHshahan <saroar.shahan@vivacomsolutions.com>
2020-05-11 06:31:57 +0600

    solved uri problem 1007 of beginner

--- uri/beginner/1007Difference.js
+++ uri/beginner/1007Difference.js
@@ -1,6 +1,9 @@
-var a = parseInt(5)
-var b = parseInt(6)
-var c = parseInt(7)
-var d = parseInt(8)
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseInt(lines.shift())
+var b = parseInt(lines.shift())
+var c = parseInt(lines.shift())
+var d = parseInt(lines.shift())
 
 console.log("DIFERENCA = " + ((a * b) - (c * d)))
